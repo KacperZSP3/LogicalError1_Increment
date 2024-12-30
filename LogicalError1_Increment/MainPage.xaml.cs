aadcfb21fde472702a2b017f6940bf7edfc922ea
KacperZSP3 <k.ksykiewicz@zsp3zamosc.pl>
2024-12-30 10:52:20 +0100

    LogicalError1_Increment with instructions

---
+++ LogicalError1_Increment/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿namespace LogicalError1_Increment;
+
+public partial class MainPage : ContentPage
+{
+    int count = 0;
+
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    private void IncrementButtonClicked(object sender, EventArgs e)
+    {
+        count++;
+        CountLabel.Text = "Count: " + (count -1);
+        
+    }
+}
---
+++ LogicalError1_Increment/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace LogicalError1_Increment
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
