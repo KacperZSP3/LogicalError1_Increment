19d4176d3bd0b1f94885a7506afb1ec3207c9b9b
KacperZSP3 <k.ksykiewicz@zsp3zamosc.pl>
2025-01-02 09:14:50 +0100

    Instructions

--- LogicalError1_Increment/MainPage.xaml.cs
+++ LogicalError1_Increment/MainPage.xaml.cs
@@ -1,5 +1,8 @@
 ﻿namespace LogicalError1_Increment;
-
+//Polecenie
+//Za każdym razem, gdy przycisk zostanie kliknięty,
+//etykieta powinna zaktualizować się, pokazując bieżący licznik,
+//zaczynając od 0.
 public partial class MainPage : ContentPage
 {
     int count = 0;
